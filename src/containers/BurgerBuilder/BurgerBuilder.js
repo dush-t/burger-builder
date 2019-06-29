ae7c94c6a274fa56fd2b814cc0d3d4f73de3c9c8
Dushyant Yadav <dushyant9309@gmail.com>
2019-06-29 14:57:50 +0530

    Setup and integrate orders page with firebase integration

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,31 +92,12 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('Order placed!')
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Dushyant Yadav',
-        //         address: {
-        //             street: "SH 2132",
-        //             zipCode: "301001",
-        //             country: "India"
-        //         },
-        //         email: "dushyant9309@gmail.com"
-        //     },
-        //     deliveryMethod: "fastest"
-        // }
-        // axios.post('/orders.json', order)
-        //     .then((response) => {
-        //         this.setState({loading: false, purchasing: false});
-        //     }).catch((error) => {
-        //         this.setState({loading: false, purchasing: false});
-        //     })
+        
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+        queryParams.push('price='+ this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
